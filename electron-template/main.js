a30919040a5813cab00e0599f870a2296a75083a
huchangzhi <hcz1017@outlook.com>
2026-01-31 15:02:21 +0800

    更新关于对话框中的版本号至 2.0.1

--- electron-template/main.js
+++ electron-template/main.js
@@ -143,7 +143,7 @@ function showAboutDialog() {
         <div class="info">
           <h2>${appName}</h2>
           <p>打包工具: <a href="#" class="link" onclick="openLink('https://github.com/huchangzhi/htmltoexe')">https://github.com/huchangzhi/htmltoexe</a></p>
-          <p>版本: 1.0.0</p>
+          <p>版本: 2.0.1</p>
         </div>
       </div>
       <script>
